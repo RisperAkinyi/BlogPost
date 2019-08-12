0e89802c8acade158c244639ce02392001af27e8
Risper Akinyi <email@example.com>
2019-08-12 12:58:51 +0300

    Add the main/error

--- app/main/error.py
+++ app/main/error.py
@@ -4,6 +4,6 @@ from . import main
 @main.app_errorhandler(404)
 def four_Ow_four(error):
     '''
-    Function to render the 404 error page
+    Function to render the 404 error pag
     '''
     return render_template('fourOfour.html'),404
