6354818dc3d74602cb7d5ba02ae485da0d831417
Risper Akinyi <email@example.com>
2019-08-12 10:24:14 +0300

    add initial folder structure

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='qwerty12345'
+export DATABASE_URL='postgresql+psycopg2://postgres:risperhub3@localhost/arisi'
+export MAIL_USERNAME='risperakinyi3@gmail.com'
+export MAIL_PASSWORD='november3'
+python3.6 manage.py server
